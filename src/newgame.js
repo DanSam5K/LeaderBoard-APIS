9b6990684fb74a147cf96d90470da683414c90af
Daniel Samuel <dansam5000@gmail.com>
2021-09-02 11:11:37 +0100

    Get token id and start a new

---
+++ src/newgame.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+const leaderBoard = () => {
+    fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'Strikers Game',
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => json)
+      .catch((error) => console.error('Error:', error));// eslint-disable-line
+  };
+
+export default leaderBoard;
